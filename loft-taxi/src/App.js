e6108e3a0079c5e52ab06a887fd6dab039db8b32
Elena Borovikova <e.borovikova@dataduck.com>
2020-01-17 00:35:39 +0300

    function for switching pages through component states created

--- loft-taxi/src/App.js
+++ loft-taxi/src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const pages = {
+    profile: <Profile/>,
+    map: <Map/>
+};
+export default class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {currentPage: pages.profile};
+        this.handleHeader = this.handleHeader.bind(this)
+    }
+
+    handleHeader(e) {
+        let current = e.target.textContent;
+        this.setState({currentPage: pages[current]})
+
+    }
+
+    render() {
+        return (
+            <div>
+                <Header func={this.handleHeader}/>
+                <h1>page</h1>
+                {this.state.currentPage}
+            </div>
+        )
+    }
 }
 
-export default App;
+class Header extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <header>
+                <nav>
+                    <button onClick={this.props.func}>profile</button>
+                    <button onClick={this.props.func}>map</button>
+                    <button onClick={this.props.func}>login</button>
+                </nav>
+            </header>
+        )
+    }
+}
+
+function Map() {
+    return (
+        <div>
+            Map gape
+        </div>
+    );
+}
+
+function Profile() {
+    return (
+        <div>
+            Profile gape
+        </div>
+    );
+}
---
+++ loft-taxi/src/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from './App';
+
+function Login() {
+    return (
+        <div className="Login">
+            Login page
+        </div>
+    );
+}
+export default Login;
